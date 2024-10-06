171bdece49736311707df600f97df95f6922481e
Pedro López Mareque <pedro.lopez.mareque@gmail.com>
2024-10-06 10:36:50 +0200

    feat: add exercise description

--- src/trees.py
+++ src/trees.py
@@ -1,5 +1,13 @@
 from typing import Self
 
+"""
+This problem was asked by Google.
+
+Given the root to a binary tree:
+    - Implement the `serialize(node)` function which serializes the node into a string.
+    - Implement the `deserialize(string)` function which deserializes the string back into the Node.
+"""
+
 
 class Node:
     def __init__(
